25fcac72cde048c06f1f64194072f37645be019e
MFERREIRASILVA <matteo.ferreirasilva@ynov.com>
2022-04-28 01:51:16 +0200

    Get API cat but no downloading

--- src/screens/Accueil.js
+++ src/screens/Accueil.js
@@ -1,10 +1,39 @@
+/* eslint-disable handle-callback-err */
 /* eslint-disable prettier/prettier */
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { useState } from "react";
+import get from "https";
 
 function Accueil() {
+  const [cat, setCat] = useState("");
+  const getCat = async () => {
+    await fetch("https://api.thecatapi.com/v1/images/search")
+    .then(response => response.json())
+    .then(async data => {
+      setCat([data[0].url, data[0].id]);
+      // readdirSync("./../tmp", (err, files) => {
+      //   console.log(files);
+      //   files.forEach(file => {
+      //     alert(file);
+      //   });
+      // });
+      // let localPath = fs.createWriteStream("./../tmp");
+      // let request = get(data[0].url, function (response) {
+      //   console.log(response);
+      //   response.pipe(localPath);
+      // })
+    });
+  };
+
+
+
+  if (!cat) {
+    getCat();
+  }
   return (
     <View style={styles.root}>
       <Text>Accueil.</Text>
+      <Text>{cat}</Text>
     </View>
   );
 }
